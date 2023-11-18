aa25a43c0b5444ab14e61f971d171c7766b36a7a
DeadMakar <ssapientiaa@gmail.com>
2023-11-18 12:06:02 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,14 +5,15 @@ import { SearchFilter } from './SearchFilter/SearchFilter';
 import { GlobalStyle } from './GlobalStyle';
 import { useEffect, useState } from 'react';
 
-export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+const initialState = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
+export const App = () => {
+  const [contacts, setContacts] = useState(initialState);
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
